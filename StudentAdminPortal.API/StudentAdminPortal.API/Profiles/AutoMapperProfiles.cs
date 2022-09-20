0402c6901b6e683800020bb3f7131e9e8ceb43f7
Kishan Shetty <shettykishan1991@gmail.com>
2022-09-20 08:44:28 +0530

    Building APIs and installed AutoMapper

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DomainModels = StudentAdminPortal.API.Domain_Models;
+using DataModels = StudentAdminPortal.API.Data_Models;
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<DataModels.Student, DomainModels.Student>()
+                .ReverseMap();
+
+            CreateMap<DataModels.Gender, DomainModels.Gender>()
+                .ReverseMap();
+
+            CreateMap<DataModels.Address, DomainModels.Address>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using StudentAdminPortal.API.Data_Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task<List<Student>> GetStudentsAsync();
+
+    }
+}
